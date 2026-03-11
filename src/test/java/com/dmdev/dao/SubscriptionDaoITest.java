9d08585ca2f4daa240447b6e4e5800356079b58f
Pawek <ripperbog@mail.ru>
2026-03-11 12:48:37 +0300

    finished testing

---
+++ src/test/java/com/dmdev/dao/SubscriptionDaoITest.java
@@ -0,0 +1,32 @@
+package com.dmdev.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubscriptionDaoTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+}
---
+++ src/test/java/com/dmdev/service/UserServiceIT.java
@@ -0,0 +1,4 @@
+package com.dmdev.service;
+
+public class UserServiceIT {
+}
